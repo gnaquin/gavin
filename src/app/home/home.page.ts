cedf0852a5945f0e97ddf1448e00812e0d391cbb
gnaquin <graham@naquin.live>
2021-12-06 01:46:09 -0600

    lots of progress, nearly done, dummy pics and data needs altering and footer needs formatting

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
+
+SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom]);
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HomePage {
 
